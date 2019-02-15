64e3460063a6d13ec488ac254d9ad32908ceffb8
asus tech <souravkumr201@gmail.com>
2019-02-16 00:43:50 +0530

    second commit

--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from .models import Todolist
 
+from .forms import TodoListForm
+
+from django.views.decorators.http import require_POST
 def index(request):
     todo_items = Todolist.objects.order_by('id')
+    form=TodoListForm()
 
-    context = {'todo_items' : todo_items}
-    return render(request,'todolist/index.html', context)
+    context = {'todo_items' : todo_items, 'form' : form }
+    return render(request,' todolist/index.html', context)
+
+@require_POST
+def addTodoItem(request):
+    form = todoListForm(request.POST)
+
+    print(request.POST['text'])
+    return redirect('index')
+
